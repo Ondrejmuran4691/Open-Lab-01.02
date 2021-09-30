f2ef6b4da133c859763b7061b92d9aa8c9aa90f8
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-09-30 18:10:16 +0200

    Add files via upload
    done

---
+++ 1.02/Open-Lab-01.02/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_01._02
+{
+    public class Exercise
+    {
+        public int Sum(int a, int b)
+        {
+           int Sum = (a + b);
+            return Sum;
+
+        }
+    }
+}
